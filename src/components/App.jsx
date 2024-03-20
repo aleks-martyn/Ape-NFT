9fd3385579e617835fce617d61816f920bdff480
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-20 10:58:36 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import HomePage from 'pages/HomePage';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <HomePage />;
 };
