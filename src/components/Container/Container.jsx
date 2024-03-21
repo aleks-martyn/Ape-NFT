15f1ee9ee9da431bf91e71aafa6ac1b3de09b932
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-21 08:31:18 +0200

    Create Container.

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,5 @@
+import { StyledContainer } from './Container.styled';
+
+export const Container = ({ children }) => (
+  <StyledContainer>{children}</StyledContainer>
+);
---
+++ src/components/Container/Container.styled.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const StyledContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 72px;
+  padding-right: 72px;
+  width: 100%;
+  text-align: center;
+
+  @media screen and (min-width: 480px) {
+    max-width: 480px;
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    max-width: 1280px;
+  }
+`;
