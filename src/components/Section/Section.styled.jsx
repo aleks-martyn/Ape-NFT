9ccecf623c98ba73fd1fc07ea0c938eb3d5076d3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-21 15:07:48 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -2,6 +2,14 @@ import styled from '@emotion/styled';
 
 export const StyledSection = styled.section`
   padding-top: 60px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 80px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-top: 120px;
+  }
 `;
 
 export const SectionTitle = styled.h2`
@@ -11,4 +19,15 @@ export const SectionTitle = styled.h2`
   font-size: 44px;
   line-height: 0.9;
   text-transform: uppercase;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 40px;
+    font-size: 80px;
+    line-height: 1;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-bottom: 80px;
+    font-size: 160px;
+  }
 `;
