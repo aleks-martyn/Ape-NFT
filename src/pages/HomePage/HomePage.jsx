95d06f687930c2181c4baeb215fe24fdc6bcf7d1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-22 23:12:27 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,22 @@
+import { Header } from 'components/Header';
+import { Hero } from 'components/Hero';
+import { AboutSection } from 'components/AboutSection';
+import { Section } from 'components/Section';
+import { Footer } from 'components/Footer';
+
 export default function HomePage() {
-  return <p>HomePage</p>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <AboutSection />
+        <Section id="m-map" title="MIND map" />
+        <Section id="faq" title="FAQ" />
+        <Section id="arts" title="COLLECTION" />
+        <Section id="mint" title="Are you in?" />
+      </main>
+      <Footer />
+    </>
+  );
 }
