135f3cfd0aaed95c2d45845777acef20a9b697ae
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-22 23:11:57 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,10 @@
 import { Container } from 'components/Container';
 import { StyledSection, SectionTitle } from './Section.styled';
 
-export const Section = ({ title, children }) => (
+export const Section = ({ id, title, children }) => (
   <StyledSection>
     <Container>
-      <SectionTitle>{title}</SectionTitle>
+      <SectionTitle id={id}>{title}</SectionTitle>
       {children}
     </Container>
   </StyledSection>
