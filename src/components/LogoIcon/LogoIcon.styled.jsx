fd66a91175e0461ff5b62997eb8c6312a49d4e82
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-22 23:10:25 +0200

    Update icons.

---
+++ src/components/LogoIcon/LogoIcon.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const StyledIcon = styled.svg`
+  width: 48px;
+  height: 32px;
+
+  @media screen and (min-width: 768px) {
+    width: 72px;
+    height: 50px;
+  }
+`;
---
+++ src/components/TwitterIcon/TwitterIcon.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const StyledIcon = styled.svg`
+  width: 16px;
+  height: 16px;
+
+  @media screen and (min-width: 768px) {
+    width: 24px;
+    height: 24px;
+  }
+`;
