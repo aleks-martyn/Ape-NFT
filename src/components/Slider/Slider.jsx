8b02ab73039d38ee86885a3ac3aeed9213c53829
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-23 22:13:47 +0200

    Create Slider.

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,14 @@
+import { ControlButtons } from 'components/ControlButtons';
+import { SliderWrap } from './Slider.styled';
+
+export const Slider = () => {
+  return (
+    <>
+      <SliderWrap>
+        <p>This is a Slider</p>
+      </SliderWrap>
+
+      <ControlButtons />
+    </>
+  );
+};
---
+++ src/components/Slider/Slider.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const SliderWrap = styled.div`
+  width: 216px;
+  height: 242px;
+  margin-left: auto;
+  margin-right: auto;
+`;
