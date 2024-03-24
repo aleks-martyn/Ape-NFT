9188336389ca01a450135eb4e55d1d2217a3a4a1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-24 20:22:42 +0200

    Create Collection Slider.

---
+++ src/components/CollectionSlider/CollectionSlider.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { CollectionList } from 'components/CollectionList';
+import { ControlButtons } from 'components/ControlButtons';
+import { SliderWrap } from './CollectionSlider.styled';
+
+export const CollectionSlider = ({ items }) => {
+  const [elements, setElements] = useState([]);
+  const [slide, setSlide] = useState(0);
+  const [touchPosition, setTouchPosition] = useState(null);
+
+  useEffect(() => {
+    setElements(items);
+  }, [items]);
+
+  const changeSlide = (direction = 1) => {
+    let slideNumber = 0;
+
+    if (slide + direction < 0) {
+      slideNumber = elements.length - 1;
+    } else {
+      slideNumber = (slide + direction) % elements.length;
+    }
+
+    setSlide(slideNumber);
+  };
+
+  const handleTouchStart = e => {
+    const touchDown = e.touches[0].clientX;
+
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = e => {
+    if (touchPosition === null) {
+      return;
+    }
+
+    const currentPosition = e.touches[0].clientX;
+    const direction = touchPosition - currentPosition;
+
+    if (direction > 10) {
+      changeSlide(1);
+    }
+
+    if (direction < -10) {
+      changeSlide(-1);
+    }
+
+    setTouchPosition(null);
+  };
+
+  return (
+    <>
+      <SliderWrap onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+        <CollectionList items={elements} slide={slide} />
+      </SliderWrap>
+
+      <ControlButtons changeSlide={changeSlide} />
+    </>
+  );
+};
---
+++ src/components/CollectionSlider/CollectionSlider.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const SliderWrap = styled.div`
+  width: 216px;
+  height: 256px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 12px;
+  overflow: hidden;
+`;
