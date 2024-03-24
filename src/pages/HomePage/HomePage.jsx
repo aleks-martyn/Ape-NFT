31a570c04435c21597624b197261f5b29b83fab4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-25 00:31:13 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,30 @@ import { Section } from 'components/Section';
 import { Footer } from 'components/Footer';
 import { MindMap } from 'components/MindMap';
 import { Slider } from 'components/Slider';
+import { CollectionSlider } from 'components/CollectionSlider';
 import { Mint } from 'components/Mint';
+import { QuestionList } from 'components/FaqSection';
+import { cards } from 'cards';
+
+import img1 from '../../images/collection-card1.jpg';
+import img2 from '../../images/collection-card2.jpg';
+import img3 from '../../images/collection-card3.jpg';
+import img4 from '../../images/collection-card4.jpg';
+import img5 from '../../images/collection-card5.jpg';
+import img6 from '../../images/collection-card6.jpg';
+import img7 from '../../images/collection-card7.jpg';
+import img8 from '../../images/collection-card8.jpg';
+
+const apes = [
+  { src: `${img1}`, alt: 'First Ape' },
+  { src: `${img2}`, alt: 'Second Ape' },
+  { src: `${img3}`, alt: 'Third Ape' },
+  { src: `${img4}`, alt: 'Fourth Ape' },
+  { src: `${img5}`, alt: 'Fifth Ape' },
+  { src: `${img6}`, alt: 'Sixth Ape' },
+  { src: `${img7}`, alt: 'Seventh Ape' },
+  { src: `${img8}`, alt: 'Eighth Ape' },
+];
 
 export default function HomePage() {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
@@ -22,13 +45,17 @@ export default function HomePage() {
         <AboutSection />
 
         <Section id="m-map" title="MIND map">
-          {isMobile && <Slider />}
-          {isTablet && <MindMap />}
+          {isMobile && <Slider items={cards} />}
+          {isTablet && <MindMap items={cards} />}
         </Section>
 
-        <Section id="faq" title="FAQ" />
+        <Section id="faq" title="FAQ">
+          <QuestionList />
+        </Section>
 
-        <Section id="arts" title="COLLECTION" />
+        <Section id="arts" title="COLLECTION">
+          <CollectionSlider items={apes} />
+        </Section>
 
         <Section id="mint" title="Are you in?">
           <Mint />
