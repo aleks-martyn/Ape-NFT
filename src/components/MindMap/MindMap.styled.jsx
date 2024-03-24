50ae2a68d3f141d8e3abe69e5657515f0295038e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-24 10:17:58 +0200

    Update MindMap.styled.jsx

--- src/components/MindMap/MindMap.styled.jsx
+++ src/components/MindMap/MindMap.styled.jsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const Wrap = styled.div`
   @media screen and (min-width: 768px) {
     display: grid;
+    grid-gap: 24px;
     margin-left: auto;
     margin-right: auto;
     grid-template-columns: 1fr 1fr;
