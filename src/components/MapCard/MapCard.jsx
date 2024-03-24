27003e1ad46d1988ce4e0330173b6c7606a81d21
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-24 10:18:45 +0200

    Update Map Card.

--- src/components/MapCard/MapCard.jsx
+++ src/components/MapCard/MapCard.jsx
@@ -1,10 +1,37 @@
-import { Wrap, CardText, CardTitle, ArrowIcon } from './MapCard.styled';
+import {
+  Wrap,
+  CardText,
+  CardTitle,
+  ArrowIcon,
+  StyledLink,
+  Linklabel,
+} from './MapCard.styled';
 
 export const MapCard = ({ title, text, index }) => {
   return (
-    <Wrap title={title}>
-      {index !== 3 ? <CardText>{text}</CardText> : <ArrowIcon />}
-      <CardTitle>{title}</CardTitle>
-    </Wrap>
+    <>
+      {index !== 3 ? (
+        <Wrap title={title}>
+          <CardText>{text}</CardText>
+
+          <CardTitle>{title}</CardTitle>
+        </Wrap>
+      ) : (
+        <StyledLink
+          href="https://en.wikipedia.org/wiki/Non-fungible_token"
+          target="_blank"
+          rel="noreferrer noopener"
+          title={title}
+        >
+          <ArrowIcon />
+
+          <Linklabel>
+            Learn
+            <br /> more
+            <br /> in mind map
+          </Linklabel>
+        </StyledLink>
+      )}
+    </>
   );
 };
--- src/components/MapCard/MapCard.styled.jsx
+++ src/components/MapCard/MapCard.styled.jsx
@@ -73,3 +73,43 @@ export const ArrowIcon = styled(NorthEastIcon)`
     width: 64px;
     height: 64px;
 `;
+
+export const StyledLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 216px;
+  height: 242px;
+  padding: 24px 12px;
+  border-radius: 12px;
+  background-color: ${({ title }) => getBgColor(title)};
+  text-decoration: none;
+  color: var(--primary-text-color);
+
+  @media screen and (min-width: 768px) {
+    width: 284px;
+    height: 242px;
+    padding: 24px;
+    border-radius: 16px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 504px;
+    height: 480px;
+    border-radius: 24px;
+  }
+`;
+
+export const Linklabel = styled.span`
+  font-family: 'Right Grotesk';
+  font-size: 32px;
+  font-weight: 900;
+  line-height: 1;
+  text-align: left;
+  text-transform: uppercase;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 64px;
+  }
+`;
