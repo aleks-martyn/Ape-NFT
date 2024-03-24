41f620b30f7394c9002ae5a6a3816d08bdd5aa9d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-24 20:21:12 +0200

    Create collection-apes.js

---
+++ src/collection-apes.js
@@ -0,0 +1,10 @@
+export const collectionApes = [
+  { src: './images/collection-card1.jpg', alt: 'First Ape' },
+  { src: './images/collection-card2.jpg', alt: 'Second Ape' },
+  { src: './images/collection-card3.jpg', alt: 'Third Ape' },
+  { src: './images/collection-card4.jpg', alt: 'Fourth Ape' },
+  { src: './images/collection-card5.jpg', alt: 'Fifth Ape' },
+  { src: './images/collection-card6.jpg', alt: 'Sixth Ape' },
+  { src: './images/collection-card7.jpg', alt: 'Seventh Ape' },
+  { src: './images/collection-card8.jpg', alt: 'Eighth Ape' },
+];
