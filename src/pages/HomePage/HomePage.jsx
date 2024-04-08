6d32eff8060e0cce9234fa35206e91e345451352
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-04-08 16:40:00 +0300

    Update text content Home Page.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,6 +10,7 @@ import { CollectionSlider } from 'components/CollectionSlider';
 import { Mint } from 'components/Mint';
 import { QuestionList } from 'components/FaqSection';
 import textContent from '../../text-content.json';
+import { createItems } from 'utils/createItems';
 
 import img1 from '../../images/collection-card1.jpg';
 import img2 from '../../images/collection-card2.jpg';
@@ -20,22 +21,13 @@ import img6 from '../../images/collection-card6.jpg';
 import img7 from '../../images/collection-card7.jpg';
 import img8 from '../../images/collection-card8.jpg';
 
-const apes = [
-  { src: `${img1}`, alt: 'First Ape' },
-  { src: `${img2}`, alt: 'Second Ape' },
-  { src: `${img3}`, alt: 'Third Ape' },
-  { src: `${img4}`, alt: 'Fourth Ape' },
-  { src: `${img5}`, alt: 'Fifth Ape' },
-  { src: `${img6}`, alt: 'Sixth Ape' },
-  { src: `${img7}`, alt: 'Seventh Ape' },
-  { src: `${img8}`, alt: 'Eighth Ape' },
-];
-
 export default function HomePage() {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
 
-  const { mapCards } = textContent;
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8];
+  const { mapCards, apeDescriptions } = textContent;
+  const apes = createItems(apeDescriptions, images);
 
   return (
     <>
