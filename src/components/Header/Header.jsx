771492bb9faa1c997b3a71aacb77bb2d955635a3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-04-22 23:51:22 +0300

    Add LinkList Component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react';
 import { ModalMenu } from 'components/ModalMenu';
+import { LinkList } from 'components/LinkList';
+import { LogoIcon } from 'components/LogoIcon';
+import { icons } from 'socialIcons';
 import {
   StyledHeader,
   StyledContainer,
   Wrap,
   InnerWrap,
-  LinkList,
   BtnLabel,
   LogoLink,
-  SocialLink,
   MenuBtn,
 } from './Header.styled';
-import { LogoIcon } from 'components/LogoIcon';
-import { TwitterIcon } from 'components/TwitterIcon';
-import { LogomarkIcon } from 'components/LogomarkIcon';
-import { DiscordIcon } from 'components/DiscordIcon';
 
 export const Header = () => {
   const [isShowModal, setIsShowModal] = useState(false);
@@ -36,37 +33,7 @@ export const Header = () => {
               <BtnLabel>MENU</BtnLabel>
             </MenuBtn>
 
-            <LinkList>
-              <li>
-                <SocialLink
-                  href="https://discord.com"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <DiscordIcon />
-                </SocialLink>
-              </li>
-
-              <li>
-                <SocialLink
-                  href="https://logomark.com"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <LogomarkIcon />
-                </SocialLink>
-              </li>
-
-              <li>
-                <SocialLink
-                  href="https://www.twitter.com"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <TwitterIcon />
-                </SocialLink>
-              </li>
-            </LinkList>
+            <LinkList items={icons} component="header" />
           </InnerWrap>
         </Wrap>
       </StyledContainer>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -7,6 +7,7 @@ export const StyledHeader = styled.header`
   left: 0;
   right: 0;
   padding-top: 62px;
+  z-index: 1;
 
   @media screen and (min-width: 768px) {
     padding-top: 66px;
@@ -56,17 +57,6 @@ export const InnerWrap = styled.div`
   }
 `;
 
-export const LinkList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  z-index: 1;
-
-  @media screen and (min-width: 1280px) {
-    gap: 15px;
-  }
-`;
-
 export const LogoLink = styled(Link)`
   display: flex;
   width: 48px;
@@ -100,30 +90,6 @@ export const BtnLabel = styled.span`
   @media screen and (min-width: 1280px) {
     font-size: 16px;
     line-height: 1.19;
-  }
-`;
-
-export const SocialLink = styled.a`
-  display: flex;
-  width: 48px;
-  height: 48px;
-  border-radius: 8px;
-  border: none;
-  background-color: var(--btn-color);
-  color: var(--secondary-text-color);
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-  transition: color var(--tra);
-
-  &:hover {
-    color: var(--primary-text-color);
-  }
-
-  @media screen and (min-width: 1280px) {
-    width: 80px;
-    height: 80px;
-    border-radius: 12px;
   }
 `;
 
