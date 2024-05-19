045337c341a00330a52eefd5f97d49a62da67cb3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-05-19 23:06:10 +0300

    Update LinkList.jsx
    Add scrollY prop.

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { useMediaQuery } from 'react-responsive';
 import { List, SocialLink } from './LinkList.styled';
 
-export const LinkList = ({ items, component }) => {
+export const LinkList = ({ items, component, scrollY }) => {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   const getIconSize = screenDimension => (screenDimension ? 16 : 24);
   const size = getIconSize(isMobile);
@@ -17,6 +17,8 @@ export const LinkList = ({ items, component }) => {
             target="_blank"
             rel="noreferrer noopener"
             component={component}
+            scrollY={scrollY}
+            index={index}
           >
             {iconFunc({ style: { width: `${size}px`, height: `${size}px` } })}
           </SocialLink>
@@ -35,4 +37,5 @@ LinkList.propTypes = {
     }).isRequired
   ).isRequired,
   component: PropTypes.string.isRequired,
+  scrollY: PropTypes.number,
 };
