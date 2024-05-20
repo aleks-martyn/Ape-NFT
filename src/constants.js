783cb4cc915cb0bf0302e07cd55664e349554515
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-05-20 17:53:00 +0300

    Update constants.js
    Add constants for link's scroll.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,11 @@
 export const TABLET_CHANGE_POINT = 386;
 
+export const TABLET_LINK_HEIGHT = 48;
+
+export const TABLET_LIST_GAP = 8;
+
 export const DESK_CHANGE_POINT = 612;
+
+export const DESK_LINK_HEIGHT = 80;
+
+export const DESK_LIST_GAP = 15;
