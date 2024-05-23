0b372c67f1cc5e875cd76163254c5c42b7c2ceab
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-05-23 22:44:15 +0300

    Update props and add scroll styles.

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -2,22 +2,22 @@ import PropTypes from 'prop-types';
 import { useMediaQuery } from 'react-responsive';
 import { List, SocialLink } from './LinkList.styled';
 
-export const LinkList = ({ items, component, scrollY }) => {
+export const LinkList = props => {
+  const { items, ...rest } = props;
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   const getIconSize = screenDimension => (screenDimension ? 16 : 24);
   const size = getIconSize(isMobile);
 
   return (
-    <List component={component}>
+    <List {...rest}>
       {items.map(({ iconFunc, url, label }, index) => (
         <li key={index}>
           <SocialLink
+            {...rest}
             aria-label={label}
             href={url}
             target="_blank"
             rel="noreferrer noopener"
-            component={component}
-            scrollY={scrollY}
             index={index}
           >
             {iconFunc({ style: { width: `${size}px`, height: `${size}px` } })}
@@ -37,5 +37,5 @@ LinkList.propTypes = {
     }).isRequired
   ).isRequired,
   component: PropTypes.string.isRequired,
-  scrollY: PropTypes.number,
+  scrolly: PropTypes.number.isRequired,
 };
--- src/components/LinkList/LinkList.styled.jsx
+++ src/components/LinkList/LinkList.styled.jsx
@@ -1,10 +1,17 @@
 import styled from '@emotion/styled';
 import { getDisplay } from 'utils/getDisplay';
 import { getColor } from 'utils/getColor';
+import { getBgColor } from 'utils/getBgColor';
 import { getHoverColor } from 'utils/getHoverColor';
-
-const getBgColor = componentName =>
-  componentName === 'header' ? 'var(--btn-color)' : 'var(--modal-btn-bg-color)';
+import {
+  MOBILE_CHANGE_POINT,
+  TABLET_CHANGE_POINT,
+  DESK_CHANGE_POINT,
+  TABLET_LINK_HEIGHT,
+  DESK_LINK_HEIGHT,
+  TABLET_LIST_GAP,
+  DESK_LIST_GAP,
+} from 'constants';
 
 export const List = styled.ul`
   display: flex;
@@ -20,30 +27,88 @@ export const List = styled.ul`
   }
 `;
 
+const mobileObjParam = {
+  changeColorPosition: MOBILE_CHANGE_POINT,
+  height: TABLET_LINK_HEIGHT,
+  gap: TABLET_LIST_GAP,
+};
+
+const tabletObjParam = {
+  changeColorPosition: TABLET_CHANGE_POINT,
+  height: TABLET_LINK_HEIGHT,
+  gap: TABLET_LIST_GAP,
+};
+
+const deskObjParam = {
+  changeColorPosition: DESK_CHANGE_POINT,
+  height: DESK_LINK_HEIGHT,
+  gap: DESK_LIST_GAP,
+};
+
+const getChangeColorPoint = (index, { changeColorPosition, height, gap }) =>
+  changeColorPosition - (index + 1) * (height + gap);
+
 export const SocialLink = styled.a`
   display: flex;
   width: 48px;
   height: 48px;
   border-radius: 8px;
   border: none;
-  background-color: ${({ component }) => getBgColor(component)};
-  color: ${({ component }) => getColor(component)};
+  background-color: ${({ component, scrolly, index }) =>
+    getBgColor(component, scrolly, getChangeColorPoint(index, mobileObjParam))};
+  color: ${({ component, scrolly, index }) =>
+    getColor(component, scrolly, getChangeColorPoint(index, mobileObjParam))};
   align-items: center;
   justify-content: center;
   padding: 0;
-  transition: color var(--tra);
 
   &:hover {
-    color: ${({ component }) => getHoverColor(component)};
+    color: ${({ component, scrolly, index }) =>
+      getHoverColor(
+        component,
+        scrolly,
+        getChangeColorPoint(index, mobileObjParam)
+      )};
+    transition: color var(--tra);
+
+    @media screen and (min-width: 768px) {
+      color: ${({ component, scrolly, index }) =>
+        getHoverColor(
+          component,
+          scrolly,
+          getChangeColorPoint(index, tabletObjParam)
+        )};
+    }
+
+    @media screen and (min-width: 1280px) {
+      color: ${({ component, scrolly, index }) =>
+        getHoverColor(
+          component,
+          scrolly,
+          getChangeColorPoint(index, deskObjParam)
+        )};
+    }
   }
 
   @media screen and (min-width: 768px) {
     display: ${({ component }) => getDisplay(component)};
+    background-color: ${({ component, scrolly, index }) =>
+      getBgColor(
+        component,
+        scrolly,
+        getChangeColorPoint(index, tabletObjParam)
+      )};
+    color: ${({ component, scrolly, index }) =>
+      getColor(component, scrolly, getChangeColorPoint(index, tabletObjParam))};
   }
 
   @media screen and (min-width: 1280px) {
     width: 80px;
     height: 80px;
     border-radius: 12px;
+    background-color: ${({ component, scrolly, index }) =>
+      getBgColor(component, scrolly, getChangeColorPoint(index, deskObjParam))};
+    color: ${({ component, scrolly, index }) =>
+      getColor(component, scrolly, getChangeColorPoint(index, deskObjParam))};
   }
 `;
